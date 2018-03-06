59701b4054e9e71c1bbe8ab784433e84d513da26
Fer <fer.blazquezl@gmail.com>
2018-03-06 12:04:00 +0100

    Funciona por favoooor

--- Chess/Debug/src/subdir.mk
+++ Chess/Debug/src/subdir.mk
@@ -8,46 +8,52 @@ CPP_SRCS += \
 ../src/BishopMoveTest.cpp \
 ../src/Board.cpp \
 ../src/Cell.cpp \
+../src/Game.cpp \
 ../src/King.cpp \
 ../src/Knight.cpp \
 ../src/Pawn.cpp \
 ../src/Piece.cpp \
 ../src/Queen.cpp \
 ../src/Rook.cpp \
-../src/chess.cpp 
+../src/chess.cpp \
+../src/main.cpp 
 
 OBJS += \
 ./src/Bishop.o \
 ./src/BishopMoveTest.o \
 ./src/Board.o \
 ./src/Cell.o \
+./src/Game.o \
 ./src/King.o \
 ./src/Knight.o \
 ./src/Pawn.o \
 ./src/Piece.o \
 ./src/Queen.o \
 ./src/Rook.o \
-./src/chess.o 
+./src/chess.o \
+./src/main.o 
 
 CPP_DEPS += \
 ./src/Bishop.d \
 ./src/BishopMoveTest.d \
 ./src/Board.d \
 ./src/Cell.d \
+./src/Game.d \
 ./src/King.d \
 ./src/Knight.d \
 ./src/Pawn.d \
 ./src/Piece.d \
 ./src/Queen.d \
 ./src/Rook.d \
-./src/chess.d 
+./src/chess.d \
+./src/main.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"/home/usuario/clase/chess-1/Chess/src" -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I"/home/usuario/Documentos/chess-1/Chess/src" -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
