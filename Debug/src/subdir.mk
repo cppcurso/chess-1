2c48cb51c97efa64c2cbdc5c80288ebaac12a22f
Sara Garcia Cuenca <sara.cuenca94@gmail.com>
2018-03-07 19:12:48 +0100

    fixing errors

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -10,13 +10,20 @@ CPP_SRCS += \
 ../src/Cell.cpp \
 ../src/Game.cpp \
 ../src/King.cpp \
+../src/KingMoveTest.cpp \
 ../src/Knight.cpp \
+../src/KnightMoveTest.cpp \
+../src/Memento.cpp \
 ../src/Pawn.cpp \
+../src/PawnMoveTest.cpp \
 ../src/Piece.cpp \
 ../src/Queen.cpp \
+../src/QueenMoveTest.cpp \
 ../src/Rook.cpp \
+../src/RookMove1.cpp \
 ../src/chess.cpp \
-../src/main.cpp 
+../src/main.cpp \
+../src/pila.cpp 
 
 OBJS += \
 ./src/Bishop.o \
@@ -25,13 +32,20 @@ OBJS += \
 ./src/Cell.o \
 ./src/Game.o \
 ./src/King.o \
+./src/KingMoveTest.o \
 ./src/Knight.o \
+./src/KnightMoveTest.o \
+./src/Memento.o \
 ./src/Pawn.o \
+./src/PawnMoveTest.o \
 ./src/Piece.o \
 ./src/Queen.o \
+./src/QueenMoveTest.o \
 ./src/Rook.o \
+./src/RookMove1.o \
 ./src/chess.o \
-./src/main.o 
+./src/main.o \
+./src/pila.o 
 
 CPP_DEPS += \
 ./src/Bishop.d \
@@ -40,20 +54,27 @@ CPP_DEPS += \
 ./src/Cell.d \
 ./src/Game.d \
 ./src/King.d \
+./src/KingMoveTest.d \
 ./src/Knight.d \
+./src/KnightMoveTest.d \
+./src/Memento.d \
 ./src/Pawn.d \
+./src/PawnMoveTest.d \
 ./src/Piece.d \
 ./src/Queen.d \
+./src/QueenMoveTest.d \
 ./src/Rook.d \
+./src/RookMove1.d \
 ./src/chess.d \
-./src/main.d 
+./src/main.d \
+./src/pila.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I"/home/usuario/Documentos/chess-1/Chess/src" -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I"/home/sara/workspace/chess-1/src" -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
