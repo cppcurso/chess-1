16258980094b8bb0206c57fe82ff9069b6dd8d6a
Sara <sara@gmail.com>
2018-03-08 14:50:31 +0100

    read solved

--- src/Storage.h
+++ src/Storage.h
@@ -16,68 +16,51 @@ class Storage {
 public:
 
 	template<typename T>
-		static void write(ofstream& file, T data) {
-		    file.write((char*) &data, sizeof(T));
+	static void write(ofstream& file, T data) {
+		file.write((char*) &data, sizeof(T));
+	}
+
+	template<typename T>
+	static void read(ifstream& file, T& data) {
+		file.read((char *) &data, sizeof(T));
+	}
+
+	static void  save(){
+		ofstream file("games", ios::binary);
+		for (int i = 0; i < 8; ++i) {
+			for (int j = 0; j < 8; ++j) {
+				if (!Board::getInstance().board[i][j].isEmpty()){
+				write( file, Board::getInstance().board[i][j].piece ->figure[0]);
+				write( file, Board::getInstance().board[i][j].piece -> figure[1]);
+				write( file, Board::getInstance().board[i][j].piece -> white);
+				write( file, Board::getInstance().board[i][j].piece -> x);
+				write( file, Board::getInstance().board[i][j].piece -> y);
+				}
+
+			}
+
+		}
+	}
+	static void restore(){
+		ifstream file("games", ios::binary);
+		for (int i = 0; i < 8; ++i) {
+			for (int j = 0; j < 8; ++j) {
+				if (!Board::getInstance().board[i][j].isEmpty()){
+					read( file, Board::getInstance().board[i][j].piece ->figure[0]);
+					read( file, Board::getInstance().board[i][j].piece -> figure[1]);
+					read( file, Board::getInstance().board[i][j].piece -> white);
+					read( file, Board::getInstance().board[i][j].piece -> x);
+					read( file, Board::getInstance().board[i][j].piece -> y);
+					cout << Board::getInstance().board[i][j].piece ->figure[0] << endl;
+					cout << Board::getInstance().board[i][j].piece ->figure[1] << endl;
+					cout << Board::getInstance().board[i][j].piece ->white << endl;
+					cout << Board::getInstance().board[i][j].piece ->x << endl;
+					cout << Board::getInstance().board[i][j].piece ->y << endl;
+				}}
 		}
 
-		template<typename T>
-		static void read(ifstream file, T& data) {
-		    file.read((char *) &data, sizeof(T));
-		}
-
-		static void  save(){
-			ofstream file("games", ios::binary);
-//			for (int i = 0; i < 8; ++i) {
-//				for (int j = 0; j < 8; ++j) {
-					//if (!Board::getInstance().board[i][j].isEmpty()){
-						write( file, Board::getInstance().board[0][0].piece ->figure[0]);
-						write( file, Board::getInstance().board[0][0].piece -> figure[1]);
-						write( file, Board::getInstance().board[0][0].piece -> white);
-						write( file, Board::getInstance().board[0][0].piece -> x);
-						write( file, Board::getInstance().board[0][0].piece -> y);
 
 
-//					} else {
-//						unsigned short temp = 0;
-//						write( "games", '0');
-//						write( "games", '0');
-//						write( "games", '0');
-//						write( "games", temp);
-//						write( "games", temp);
-//					}
-				//}
-			//}
-		}
-		static void restore(){
-			ifstream file("games", ios::binary);
-			char a,b;
-			unsigned short int x,y;
-			bool w;
-//		for (int i = 0; i < 8; ++i) {
-//		for (int j = 0; j < 8; ++j) {
-//			if (!Board::getInstance().board[i][j].isEmpty()){
-
-//				Board::getInstance().board[0][0].piece = new Bishop(true, 0, 0);
-			read( file, a);
-			read( file, b);
-			read( file, w);
-			read( file, x);
-			read( file, y);
-//			} else {
-//				unsigned short temp = 0;
-//				read( "games", '0');
-//				read( "games", '0');
-//				read( "games", '0');
-//				read( "games", temp);
-//				read( "games", temp);
-//			}
-//		}
-			cout << Board::getInstance().board[0][0].piece ->figure[0] << endl;
-						cout << Board::getInstance().board[0][0].piece ->figure[1] << endl;
-						cout << Board::getInstance().board[0][0].piece ->white << endl;
-						cout << Board::getInstance().board[0][0].piece ->x << endl;
-						cout << Board::getInstance().board[0][0].piece ->y << endl;
-				//return *(Board::getInstance().board[0][0].piece);
 	}
 };
 #endif /* SRC_STORAGE_H_ */
