c9a64e5dc987921e42b7b7c6502719cdeecf7561
JorgeLanch <jorlan94@hotmail.es>
2018-03-08 11:46:44 +0100

    Fixxed rook and bishop errors

--- src/BishopMoveTest.cpp
+++ src/BishopMoveTest.cpp
@@ -10,13 +10,16 @@
 
 class BishopTest : public ::testing::Test{
 protected:
-	Bishop bishop1 = Bishop(true, 2, 0);
+	Bishop bishop1 = Bishop(true, 6, 2);
 
 
 
 };
 
 TEST_F(BishopTest, validMove){
-		ASSERT_FALSE(bishop1.validMove(5,0));
-		ASSERT_TRUE(bishop1.validMove(5,3));
+//		ASSERT_TRUE(bishop1.validMove(7,7));
+		ASSERT_TRUE(bishop1.validMove(4,4));
+		ASSERT_TRUE(bishop1.validMove(3,5));
+//		ASSERT_TRUE(bishop1.validMove(7,3));
+//		ASSERT_FALSE(bishop1.validMove(5,3));
 }
