6612c77ba3491e542861bed4e9df6d466feabe98
Sara <sara@gmail.com>
2018-03-09 09:41:34 +0100

    Storage completed

--- src/Storage.h
+++ src/Storage.h
@@ -10,6 +10,13 @@
 #include <fstream>
 #include <iostream>
 #include "Board.h"
+#include "Pawn.h"
+#include "Knight.h"
+#include "Rook.h"
+#include "King.h"
+#include "Queen.h"
+#include "Bishop.h"
+#include "chess.h"
 using namespace std;
 
 class Storage {
@@ -25,42 +32,70 @@ public:
 		file.read((char *) &data, sizeof(T));
 	}
 
-	static void  save(){
+	static void  save(int turnNumber){
 		ofstream file("games", ios::binary);
 		for (int i = 0; i < 8; ++i) {
 			for (int j = 0; j < 8; ++j) {
-				if (!Board::getInstance().board[i][j].isEmpty()){
-				write( file, Board::getInstance().board[i][j].piece ->figure[0]);
-				write( file, Board::getInstance().board[i][j].piece -> figure[1]);
-				write( file, Board::getInstance().board[i][j].piece -> white);
-				write( file, Board::getInstance().board[i][j].piece -> x);
-				write( file, Board::getInstance().board[i][j].piece -> y);
+				if (!Board::getInstance().board[j][i].isEmpty()){
+				write( file, Board::getInstance().board[j][i].piece ->figure[0]);
+				write( file, Board::getInstance().board[j][i].piece -> white);
+				write( file, Board::getInstance().board[j][i].piece -> x);
+				write( file, Board::getInstance().board[j][i].piece -> y);
 				}
 
 			}
-
-		}
+		} write (file, turnNumber);
 	}
-	static void restore(){
+	static int restore(){
 		ifstream file("games", ios::binary);
-		for (int i = 0; i < 8; ++i) {
-			for (int j = 0; j < 8; ++j) {
-				if (!Board::getInstance().board[i][j].isEmpty()){
-					read( file, Board::getInstance().board[i][j].piece ->figure[0]);
-					read( file, Board::getInstance().board[i][j].piece -> figure[1]);
-					read( file, Board::getInstance().board[i][j].piece -> white);
-					read( file, Board::getInstance().board[i][j].piece -> x);
-					read( file, Board::getInstance().board[i][j].piece -> y);
-					cout << Board::getInstance().board[i][j].piece ->figure[0] << endl;
-					cout << Board::getInstance().board[i][j].piece ->figure[1] << endl;
-					cout << Board::getInstance().board[i][j].piece ->white << endl;
-					cout << Board::getInstance().board[i][j].piece ->x << endl;
-					cout << Board::getInstance().board[i][j].piece ->y << endl;
-				}}
-		}
+		int t;
+		char a;
+		bool w;
+		short x,y;
+		for (int i = 0; i < 32; ++i) {
+			read( file, a);
+			read( file, w);
+			read( file, x);
+			read( file, y);
+			cout<<"a: "<<a<<endl;
+			cout<<"x:"<<x<<endl;
+			cout<<"y: "<<y<<endl;
+			switch(a){
+			case 'P':
+				Board::getInstance().board[x][y] = Cell(new Pawn(w,x,y));
+				break;
+			case 'T':
+				Board::getInstance().board[x][y] = Cell(new Rook(w,x,y));
+				break;
+			case 'K':
+				Board::getInstance().board[x][y] = Cell(new King(w,x,y));
+				break;
+			case 'k':
+				Board::getInstance().board[x][y] = Cell(new Knight(w,x,y));
+				break;
+			case 'Q':
+				Board::getInstance().board[x][y] = Cell(new Queen(w,x,y));
+				break;
+			case 'B':
+				Board::getInstance().board[x][y] = Cell(new Bishop(w,x,y));
+				break;
+			}
 
-
-
+		}read( file, t);
+		return t;
 	}
 };
 #endif /* SRC_STORAGE_H_ */
+
+//				if (!Board::getInstance().board[i][j].isEmpty()){
+			//					read( file, Board::getInstance().board[i][j].piece ->figure[0]);
+			//					read( file, Board::getInstance().board[i][j].piece -> figure[1]);
+			//					read( file, Board::getInstance().board[i][j].piece -> white);
+			//					read( file, Board::getInstance().board[i][j].piece -> x);
+			//					read( file, Board::getInstance().board[i][j].piece -> y);
+			//					cout << Board::getInstance().board[i][j].piece ->figure[0] << endl;
+			//					cout << Board::getInstance().board[i][j].piece ->figure[1] << endl;
+			//					cout << Board::getInstance().board[i][j].piece ->white << endl;
+			//					cout << Board::getInstance().board[i][j].piece ->x << endl;
+			//					cout << Board::getInstance().board[i][j].piece ->y << endl;
+			//				}
--- src/main.cpp
+++ src/main.cpp
@@ -14,17 +14,8 @@ int main(int argc, char **argv) {
 //	::testing::InitGoogleTest(&argc, argv);
 //	return RUN_ALL_TESTS();
 
-	Board::getInstance();
-		Board::getInstance().init();
-//		Board::getInstance().print();
-		Storage::save();
-		Storage::restore();
-		Board::getInstance().move(0, 1, 0, 2);
-//		Board::getInstance().print();
-
-
-//	Chess chessGame;
-//	chessGame.play();
+	Chess chessGame;
+	chessGame.play();
 
 }
 
